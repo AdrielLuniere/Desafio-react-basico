0cdd01e11b3fecf218f984f0a50770964ef0ff31
AdrielLuniere <107431134+AdrielLuniere@users.noreply.github.com>
2023-01-06 19:39:11 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Text from './components/text/index'
+import Button from './components/button/index'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Button />
+    <Text />
+    </>
+  );
+}
+
+export default App;
